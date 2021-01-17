57256774998c1ae75a189726588de73325f0b7c5
MariaSyrokvash <mariya.syrokvash@gmail.com>
2021-01-17 15:58:06 +0300

    add confirming pass

--- src/components/newPassword/NewPassword.tsx
+++ src/components/newPassword/NewPassword.tsx
@@ -21,23 +21,35 @@ type ParamTypes = {
 
 export const NewPassword = (props: NewPasswordType) => {
 	const [password, setPassword] = useState<string>('')
+	const [confirmedPassword, setConfirmedPassword] = useState<string>('')
 	const [disabled, setDisabled] = useState<boolean>(true)
+	const [matchError, setMatchError] = useState<boolean>(false)
 	const dispatch = useDispatch();
 	const {token} = useParams<ParamTypes>()
 	console.log(token)
 
 	const onChangeInputHandler = (event: ChangeEvent<HTMLInputElement>) => {
 		setDisabled(false)
+		setMatchError(false)
 		event.currentTarget.value && setPassword(event.currentTarget.value)
 		dispatch(setErrorAC(''))
 	}
 
+	const onChangeInputConfirmedHandler = (event: ChangeEvent<HTMLInputElement>) => {
+		setMatchError(false)
+		event.currentTarget.value && setConfirmedPassword(event.currentTarget.value)
+	}
+
 	const onClickButtonHandler = () => {
+		if (password !== confirmedPassword) {
+			return setMatchError(true)
+		}
+
 		dispatch(sendNewPasswordTC(password, token))
 		setDisabled(true)
 		setPassword('')
+		setConfirmedPassword('')
 	}
-
 
 	const onEnter = () => {
 		if (!props.error) {
@@ -59,8 +71,17 @@ export const NewPassword = (props: NewPasswordType) => {
 	return (
 		<div className={s.box}>
 			<h1>New password</h1>
-			<SuperInputText placeholder='New password' value={password} onChange={onChangeInputHandler} type='password'
-											error={props.error} onKeyPress={onKeyPressHandler}/>
+			<SuperInputText placeholder='New password' value={password} onChange={onChangeInputHandler}
+											error={props.error} onKeyPress={onKeyPressHandler} type='password'/>
+
+			{
+				props.error &&
+        <>
+          <SuperInputText placeholder='Confirm password' value={confirmedPassword} error={props.error}
+                          onChange={onChangeInputConfirmedHandler} type='password'/>
+					{matchError ? 'Passwords don\'t match!' : ''}
+        </>
+			}
 			<SuperButton disabled={disabled} onClick={onClickButtonHandler} className={s.newPassBtn}>Save</SuperButton>
 		</div>
 	);
