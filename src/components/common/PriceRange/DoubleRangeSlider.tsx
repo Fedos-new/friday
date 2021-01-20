5f2a99b244f534919f90a725199fd31f8ba927bc
MariaSyrokvash <mariya.syrokvash@gmail.com>
2021-01-21 01:57:20 +0300

    feat: add slider component

--- src/components/common/PriceRange/DoubleRangeSlider.tsx
+++ src/components/common/PriceRange/DoubleRangeSlider.tsx
@@ -1,9 +1,6 @@
-import React, {ChangeEvent, useState} from 'react';
+import React, {ChangeEvent} from 'react';
 import {Slider} from '@material-ui/core';
 import s from './PriceRange.module.css'
-import {useDispatch, useSelector} from 'react-redux';
-import {setMinMaxPriceRangeAC} from '../../../bll/search-reducer';
-import {AppRootState} from '../../../bll/store';
 import {withStyles} from '@material-ui/styles';
 
 const StyledSlider = withStyles({
@@ -23,31 +20,23 @@ const StyledSlider = withStyles({
 	}
 })(Slider);
 
-export const PriceRange = () => {
-	const dispatch = useDispatch()
-	const minPrice = useSelector<AppRootState, number>(state => state.search.settings.minPrice)
-	const maxPrice = useSelector<AppRootState, number>(state => state.search.settings.maxPrice)
-	const [value1, setValue1] = useState(1000);
-	const [value2, setValue2] = useState(9000);
-	const value = [value1, value2]
-
-	const onChangeRange = (value: number | [number, number]) => {
-		if (Array.isArray(value)) {
-			setValue1(value[0])
-			setValue2(value[1])
-			dispatch(setMinMaxPriceRangeAC(value[0], value[1]))
-		}
-	}
+type DoubleRangeSliderType = {
+	minPrice: number
+	maxPrice: number
+	valueArray:  Array<number>
+	onChangeRange: (value: number | [number, number]) => void
+}
+export const DoubleRangeSlider: React.FC<DoubleRangeSliderType> = ({minPrice, maxPrice, valueArray, onChangeRange}) => {
 
 	const onChangeHandler = (event: ChangeEvent<{}>, value: (number[] | number)) => {
 		onChangeRange && onChangeRange(value as number)
 	}
 
 	return (
-		<div className={s.box}>
+		<div className={s.wrap}>
 			<p className={s.minPrice}>{minPrice}</p>
 			<StyledSlider
-				value={value}
+				value={valueArray}
 				onChange={onChangeHandler}
 				valueLabelDisplay="auto"
 				aria-labelledby="range-slider"
--- src/components/common/PriceRange/PriceRange.tsx
+++ src/components/common/PriceRange/PriceRange.tsx
@@ -1,60 +1,30 @@
-import React, {ChangeEvent, useState} from 'react';
-import {Slider} from '@material-ui/core';
+import React, {useState} from 'react';
 import s from './PriceRange.module.css'
 import {useDispatch, useSelector} from 'react-redux';
 import {setMinMaxPriceRangeAC} from '../../../bll/search-reducer';
 import {AppRootState} from '../../../bll/store';
-import {withStyles} from '@material-ui/styles';
-
-const StyledSlider = withStyles({
-	root: {
-		color: '#4c84e0',
-	},
-	thumb: {
-		height: 16,
-		width: 16,
-		backgroundColor: '#4c84e0',
-		border: '2px solid #4c84e0',
-		marginTop: -8,
-		marginLeft: -12,
-		'&:focus, &:hover, &$active': {
-			boxShadow: 'inherit',
-		},
-	}
-})(Slider);
+import {DoubleRangeSlider} from './DoubleRangeSlider';
 
 export const PriceRange = () => {
 	const dispatch = useDispatch()
 	const minPrice = useSelector<AppRootState, number>(state => state.search.settings.minPrice)
 	const maxPrice = useSelector<AppRootState, number>(state => state.search.settings.maxPrice)
-	const [value1, setValue1] = useState(1000);
-	const [value2, setValue2] = useState(9000);
-	const value = [value1, value2]
+	const [minValue, setMinValue] = useState(minPrice);
+	const [maxValue, setMaxValue] = useState(maxPrice);
+	const valueArray = [minValue, maxValue]
 
 	const onChangeRange = (value: number | [number, number]) => {
 		if (Array.isArray(value)) {
-			setValue1(value[0])
-			setValue2(value[1])
+			setMinValue(value[0])
+			setMaxValue(value[1])
 			dispatch(setMinMaxPriceRangeAC(value[0], value[1]))
 		}
 	}
 
-	const onChangeHandler = (event: ChangeEvent<{}>, value: (number[] | number)) => {
-		onChangeRange && onChangeRange(value as number)
-	}
-
 	return (
 		<div className={s.box}>
-			<p className={s.minPrice}>{minPrice}</p>
-			<StyledSlider
-				value={value}
-				onChange={onChangeHandler}
-				valueLabelDisplay="auto"
-				aria-labelledby="range-slider"
-				min={minPrice}
-				max={maxPrice}
-			/>
-			<p className={s.maxPrice}>{maxPrice}</p>
+			<h1>Price Range</h1>
+			<DoubleRangeSlider minPrice={minPrice} maxPrice={maxPrice} valueArray={valueArray} onChangeRange={onChangeRange}/>
 		</div>
 	);
 }
